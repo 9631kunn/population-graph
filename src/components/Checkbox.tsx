7cbe0797de906d0d392f249d041f78c5bf86d3a5
9631kunn <infourbh+1@gmail.com>
2021-08-02 23:46:26 +0900

    checkboxの押下時に識別IDを配列に挿入

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,9 +1,22 @@
+import { useRecoilState } from 'recoil'
 import { ResponsePrefecture } from '../models/Api'
+import { checkedPrefecturesState } from '../atoms/checkedPrefectures'
 
 const Checkbox = ({ pref }: { pref: ResponsePrefecture }): JSX.Element => {
+  const { prefCode } = pref
+
+  const [checkedPrefectures, setCheckedPrefectures] = useRecoilState(
+    checkedPrefecturesState
+  )
+
+  const handleChange = () => {
+    setCheckedPrefectures([...checkedPrefectures, prefCode])
+    console.log(checkedPrefectures)
+  }
+
   return (
     <label>
-      <input type="checkbox" />
+      <input type="checkbox" onClick={() => handleChange()} />
       {pref.prefName}
     </label>
   )
